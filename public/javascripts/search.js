c0329a223ddb7740c9fcac8dea31e0b9103f8092
Vijay Korapaty <vijay.korapaty@whitehatsec.com>
2013-01-22 09:54:40 -0800

    Fix for search not working on page load implemented, search results appear a bit more smoothly, and empty submitting an empty search field will reset the page.

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -23,65 +23,69 @@
 
 
     $('#searchForm').submit( function()  {
-        //console.log(searchResults);
         clearSearch();
-        $("#search1").catcomplete("close");
-        $('li.endpoint > h3.title span.name').each(function () {
-            if(!matching($(this).text(), searchResults)) {
-                $(this).parent().parent().addClass('hide-this');
-            }
-            else {
-                // endpoint name matched
-                //console.log($(this).text());
+        if ( $('#search1').val().length > 0 ) {
+            $("#search1").catcomplete("close");
+            $('li.endpoint > h3.title span.name').each(function () {
+                if(!matching($(this).text(), searchResults)) {
+                    $(this).parent().parent().addClass('hide-this');
+                }
+                else {
+                    // endpoint name matched
+                    //console.log($(this).text());
 
-                $('ul.methods', this.parentNode.parentNode).slideToggle();
-                $(this.parentNode.parentNode).toggleClass('expanded')
+                    // Deal with hiding methods that did not match. 
+                    $(this.parentNode).siblings('ul.methods').children('li.method').each(function () {
+                        if (!matching($(this).find('div.title span.name').text(), searchResults)) {
+                            //console.log($(this).find('div.title span.name').text());
+                            $(this).addClass('hide-this');
+                        }
+                        else {
+                            //console.log($(this).find('div.title span.name').text());
+                        }
+                    });
 
-                // Deal with hiding methods that did not match. 
-                $(this.parentNode).siblings('ul.methods').children('li.method').each(function () {
-                    if (!matching($(this).find('div.title span.name').text(), searchResults)) {
-                        //console.log($(this).find('div.title span.name').text());
-                        $(this).addClass('hide-this');
-                    }
-                    else {
-                        //console.log($(this).find('div.title span.name').text());
-                    }
-                });
-            }
-        });
+                    // Toggle endpoint
+                    $('ul.methods', this.parentNode.parentNode).slideToggle();
+                    $(this.parentNode.parentNode).toggleClass('expanded')
+                }
+            });
+        }
+        return false;
+
     });
 
     $('#clear-search').click(function () {
         clearSearch();
     });
 
-function matching (text, searchResults) {
-    for (var k = 0; k < searchResults.length; k++) {
-        for ( var prop in searchResults[k] ) {
-            if (searchResults[k][prop] == text) {
-                // Match found
-                return true;
+    function matching (text, searchResults) {
+        for (var k = 0; k < searchResults.length; k++) {
+            for ( var prop in searchResults[k] ) {
+                if (searchResults[k][prop] == text) {
+                    // Match found
+                    return true;
+                }
             }
         }
+        // No matches found.
+        return false;
     }
-    // No matches found.
-    return false;
-}
+
+    function clearSearch () {
+        $('li.method.hide-this').removeClass('hide-this');
+        // Close open endpoints
+        var endpoints = $('ul.methods'),
+            endpointsLength = endpoints.length;
+
+        for (var x = 0; x < endpointsLength; x++) {
+            var methodsList = $(endpoints[x]);
+            methodsList.slideUp();
+            methodsList.parent().toggleClass('expanded', false)
+        }
+        $('li.endpoint.hide-this').removeClass('hide-this');
+    }
+
 })();
 
-
-
-function clearSearch () {
-    $('li.method.hide-this').removeClass('hide-this');
-    // Close open endpoints
-    var endpoints = $('ul.methods'),
-        endpointsLength = endpoints.length;
-
-    for (var x = 0; x < endpointsLength; x++) {
-        var methodsList = $(endpoints[x]);
-        methodsList.slideUp();
-        methodsList.parent().toggleClass('expanded', false)
-    }
-    $('li.endpoint.hide-this').removeClass('hide-this');
-}
 
