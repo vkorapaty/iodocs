683ff0e3dc44fb6dc6f12ec47d5c313d13e70537
Vijay Korapaty <vijay.korapaty@whitehatsec.com>
2013-01-28 11:04:54 -0800

    Drop downs now sort of works in PUT and POST methods, needs refining.

--- public/javascripts/contentparams.js
+++ public/javascripts/contentparams.js
@@ -8,8 +8,58 @@
         // complicated data types. 
         fieldVal = $(this).val();
 
+        updateTextArea( $(this), fieldName, fieldVal );
+    });
+
+    // Keyup listener for all of the content parameter input fields (content parameter input fields should belong only to PUT or POST methods)
+    $('.content > table.parameters > tbody > tr > td.parameter select').click(function (event) {
+        
+        // Obtain the parameter name and parameter value
+        fieldName = $(this).parent().siblings('.name').text();
+        // Will want to do some processing here in the future for more
+        // complicated data types. 
+        if ( $(this).val() == 'value' ) { 
+            fieldVal = true;
+        }
+        else if ( $(this).val() == 'false' ) {
+            fieldVal = false;
+        }
+        else if ( /^\d+$/.test($(this).val()) ) {
+            fieldVal = parseInt($(this).val());
+        }
+        else {
+            fieldVal = $(this).val();
+        }
+
+        updateTextArea( $(this), fieldName, fieldVal );
+    });
+
+    // Keyup listener for all of the content parameter input fields (content parameter input fields should belong only to PUT or POST methods)
+    $('.content > table.parameters > tbody > tr > td.parameter select').keyup(function (event) {
+        
+        // Obtain the parameter name and parameter value
+        fieldName = $(this).parent().siblings('.name').text();
+        // Will want to do some processing here in the future for more
+        // complicated data types. 
+        if ( $(this).val() == 'value' ) { 
+            fieldVal = true;
+        }
+        else if ( $(this).val() == 'false' ) {
+            fieldVal = false;
+        }
+        else if ( /^\d+$/.test($(this).val()) ) {
+            fieldVal = parseInt($(this).val());
+        }
+        else {
+            fieldVal = $(this).val();
+        }
+
+        updateTextArea( $(this), fieldName, fieldVal );
+    });
+
+    function updateTextArea ( element, name, value ) {
         // Determine the text area to which this parameter belongs
-        var goal = $(this).closest('li.method')
+        var goal = element.closest('li.method')
                         .children('form')
                         .children('div.content')
                         .children('div.fields')
@@ -32,5 +82,5 @@
 
         // Update the text field
         goal.val(JSON.stringify(textAreaObj, null, 2));
-    });
+    }
 })();
