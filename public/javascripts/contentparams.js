cd8773015af6789ab31cb2261b12305638b6c58c
Vijay Korapaty <vijay.korapaty@whitehatsec.com>
2013-04-04 14:22:43 -0700

    Updating contentparams.js and content_parameters.jade with the contents of their alternate versions, probably unnecessary, but why not.

--- public/javascripts/contentparams.js
+++ public/javascripts/contentparams.js
@@ -1,32 +1,32 @@
 (function() {
+    // When user clicks on a PUT or POST bar, populate the text area with
+    // existing non-empty fields.
+    $('li.method.post, li.method.put').on('click', function(event) {
+        event.stopPropagation();
+        updateContentBody($(this).children('form').children('div.content'));
+    });
+ 
     // On click or data entry/change in content parameters, this block will capture
     // the event and things will happen, dominos will fall, and pigs will fly.
-    $('.content').find('td.parameter').on( "change click keyup", "input, select", function(event) {
+    $('.content').find('ul.parameters').on( "change click keyup", "input, select", function(event) {
         event.stopPropagation();
-        tree($(this));
+        updateContentBody($(this));
     });
 
-    function tree( elem ) {
+    function updateContentBody( elem ) {
         var root = elem.closest('div.content');
-        var table = root.children('table.parameters');
-        var snapshotObject = handleTable(table); 
+        var paramList = root.children('ul.parameters');
+        var snapshotObject = handleTable(paramList); 
         updateTextArea( elem, snapshotObject );
     }
 
     /*
-     The model for this is that each row of a given table is a property.
-     handleTable, handleCollection, handleList, rowName, and rowValue all 
-     work off of the basis that a row has 4 particular fields: name, parameter,
-     type, and description. Collections, objects, and lists set themselves up
-     as tables within the parameter field of a given row;
-     as such, whenever a row that has a type of 'collection', 'object', or 'list'
-     is encoutered, the contents of the parameter field for that row can be
-     operated on recursively.
+       li > ul are the row abstraction. Take the li, and then process the sub-ul.
     */
 
-    function handleTable( table , type ) {
+    function handleTable( paramList , type ) {
         if (type == 'collection') {
-            var collectionValue = handleCollection(table.children('tbody'));
+            var collectionValue = handleCollection(paramList.children('li'));
             if (collectionValue.length > 0) {
                 return collectionValue;
             }
@@ -36,7 +36,7 @@
         }
 
         if (type == 'object') {
-            var objectValue = handleObject(table.children('tbody'));
+            var objectValue = handleObject(paramList.children('li'));
             if (Object.keys( objectValue ).length !== 0) {
                 return objectValue;
             }
@@ -46,7 +46,7 @@
         }
 
         if (type == 'list') {
-            var listValue = handleList(table.children('tbody'));
+            var listValue = handleList(paramList.children('li'));
             if (listValue.length > 0) {
                 return listValue;
             }
@@ -55,27 +55,54 @@
             }
         }
 
-        var rows = table.children('tbody').children('tr');
+        if (type == 'list-only') {
+            var listValue = handleList(paramList.children('li'));
+            if (listValue.length > 0) {
+                return listValue;
+            }
+            else {
+                return;
+            }
+        }
+
+        var rows = paramList.children('li');
         var obj = {};
 
         rows.each(function () {
-            var name = rowName( $(this) );
-            var val = rowValue( $(this) );
+            var row = $(this).children('ul');
 
-            $.extend(obj, createSimpleObject(name, val));
+            var type = row.children('li.type').text();
+            type = type.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+
+            if ( row.hasClass('header') ) {
+                // Do nothing, ignore the header row.
+            }
+            else {
+                var name = rowName( row );
+                var val = rowValue( row );
+            }
+
+            if (type == 'list-only') {
+                var test =  rowValue(row);
+                obj = test;
+            }
+            else {
+                $.extend(obj, createSimpleObject(name, val));
+            }
         });
 
         return obj;
     }
 
-    function handleCollection( element ) {
+    function handleCollection( elements ) {
         // Collect information of all collection elements
         var collectionsArray = [];
         // What are the children I'm getting? Rows.
-        element.children().each( function (event) {
+        // Represented as li > ul, where the ul is the row wrapper over the row cells.
+        elements.children().each( function (event) {
             var val = rowValue( $(this) );
             var name = rowName( $(this) );
-            var attr = $(this).attr('class');
+            var attr = $(this).parent().attr('class');
             // This is to ignore the row which contains the minimize collection
             // button.
             if ( attr != '' &&  undefined != attr && undefined !== val ) {
@@ -86,12 +113,13 @@
         return createCollectionValue( collectionsArray );
     }
 
-    function handleObject( element ) {
+    function handleObject( elements ) {
         // Collect all of the properties and property values for the object
-        // element.
+        // elements.
         var tempObj = {};
         // What are the children I'm getting? Rows.
-        element.children().each( function (event) {
+        // Represented as li > ul, where the ul is the row wrapper over the row cells.
+        elements.children().each( function (event) {
             var val = rowValue( $(this) );
             var name = rowName( $(this) );
 
@@ -101,15 +129,17 @@
         return tempObj;
     }
 
-    function handleList ( element ) {
+    function handleList ( elements ) {
+        // Unlike handleObject and HandleCollection, handleList gets the set of
+        // 'li' elements that contain values to be processed. rowValue is not
+        // needed here, provided that only the basic input types are present
+        // (string/integer/enumerated).
+        //
         // Collect information of all list elements
         var collectionsArray = [];
-        element.children().each( function (event) {
-            var val = rowValue( $(this) );
-            var attr = $(this).attr('class');
-            // This is to ignore the row which contains the minimize
-            // list button.
-            if ( attr != '' &&  undefined != attr ) {
+        elements.children().each( function (event) {
+            var val = formatValue($(this).val());
+            if (val) {
                 collectionsArray.push( val );
             }
         });
@@ -118,21 +148,19 @@
     }
 
     function rowName( row ) {
-        return name = row.children('td.name')
-            .text()
-            .replace(/Add\s+\w+/g, '');
+        return name = row.children('li.name') .text();
     }
 
     function rowValue( row ) {
-        var type = row.children('td.type').text();
+        var type = row.children('li.type').text();
         type = type.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
 
         // '' option for list type, where parameter fields do not have an
         // associated type field. Could add type field back and not have this
         // problem. Or the ''== type can be the final else clause.
-        if (type == 'enumerated' || type == 'string' || type == 'integer' || '' == type) {
+        if (type == 'boolean' || type == 'enumerated' || type == 'string' || type == 'integer' || type == '') {
             var val;
-            val = row.children('td.parameter')
+            val = row.children('li.parameter')
                 .children('input, select')
                 .val();
 
@@ -141,9 +169,13 @@
             }
         }
 
-        else if (type == 'collection' || type == 'object' || type == 'list') {
-            var table  = row.children('td.parameter').children('table.parameters');
-            return handleTable(table, type);
+        else if (type == 'collection' || type == 'object' ) {
+            var paramList  = row.children('li.parameter').children('ul.parameters');
+            return handleTable(paramList, type);
+        }
+        else if (type == 'list' || type == 'list-only') {
+            var paramList  = row.children('li.parameter').children('ul');
+            return handleTable(paramList, type);
         }
     }
 
@@ -252,89 +284,67 @@
     //
     // Adding new collections to the page
     // 
-    // First thing, add 'add' button to parameters of type 'collection'
-    $("td.type:contains('collection')").each(function() {
-        $(this).siblings('td.name')
-            .append("<br/><a href='#' class='add-collection button add' onclick='return false'><img src='/images/plus.png'></img></a>");
-    // The onclick='return false' may be no good.
-    });
     
-    // What I'm looking at doing:
-    // *Click 'Add collection'
-    // *Clone node and sub-nodes. (basically clone the row that the node 
-    // belongs to)
-    //      - What exactly am I aiming to copy?
-    //          = The *rows* of the collection table, not the collection row, 
-    //          not the collection table.
-    //      ^ Implementation details
-    //          & Get tbody
-    //          & Get appropriate rows from this tbody
-    //          & Sounds like the place for the recursion to occur
-    //              & Have to check the types of the rows within...
-    //          & Keep the minimize row <--- IMPORTANT, minimize row looks like
-    //          it's implemented generally enough for it to not break stuff.
-    //          Note that there should only be one minimize row, or something
-    //          along those lines for each add... this should probably be fleshed
-    //          out more.
-    // *Change collection-original for top node to appropriate class
-    // *Remove unnecessary sub-nodes. (anything that isn't collection-original
-    // in the sub-notes/sub-rows)
-    // *Put updated block into correct position. 
-    $('td.name').on('click', 'a.add-collection', function( event ) {
+    /*
+        Docs, need docs.      
+    */
+    $('li.name').on('click', 'a.add-collection, a.add-list', function( event ) {
         event.stopPropagation();
+        if ( $(this).hasClass('add-collection') ) {
 
-        var collectionClass = $(this).parent()
-                            .siblings('td.parameter')
-                            .children('table.parameters')
-                            .children('tbody')
-                            .children(':first')
-                            .attr('class');
+            var collectionClass = $(this).parent()
+                                .siblings('li.parameter')
+                                .children('ul.parameters')
+                                .children(':first')
+                                .attr('class');
 
-        collectionClass = collectionClass.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
-        
-        var collectionBody = $(this).parent()
-                            .siblings('td.parameter')
-                            .children('table.parameters')
-                            .children('tbody')
-                            .clone(true, true);
+            collectionClass = collectionClass.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+            
+            var collectionBody = $(this).parent()
+                                .siblings('li.parameter')
+                                .children('ul.parameters')
+                                .clone(true, true);
 
-        // Obtain identifier class of the current last element in the list of
-        // collections
-        var lastRow = $(this).parent().siblings('td.parameter')
-            .children('table.parameters')
-            .children('tbody')
-            .children()
-            .last();
+            // Obtain identifier class of the current last element in the list of
+            // collections
+            var lastRow = $(this).parent().siblings('li.parameter')
+                .children('ul.parameters')
+                .children()
+                .last();
 
-        // Determine the numeric identifier from the class, then increment and
-        // pass on value
-        var collectionCount = 0;
-        if (lastRow.hasClass('collection-original')) { 
-            collectionCount = 1;
+            // Determine the numeric identifier from the class, then increment and
+            // pass on value
+            var collectionCount = 0;
+            if (lastRow.hasClass('collection-original')) { 
+                collectionCount = 1;
+            }
+            else {
+                collectionCount = parseInt(lastRow.attr('class').replace(/collection-new-/g, ''));
+                collectionCount++;
+            }
+
+            newCollectionObject(collectionBody, collectionClass, collectionCount);
+
+            $(this).parent().siblings('li.parameter')
+                        .children('ul.parameters')
+                        .append(collectionBody.children());
         }
-        else {
-            collectionCount = parseInt(lastRow.attr('class').replace(/collection-new-/g, ''));
-            collectionCount++;
+        else if ( $(this).hasClass('add-list') ){
+            // Get the 'ul' element in li.parameter for this 'list' type row.
+            var listElement = $(this).parent().siblings('li.parameter').children('ul');
+            // Make a copy of the first list element, and then append that to the end of the list.
+            var firstListElement = listElement.children().first().clone();
+            firstListElement.appendTo(listElement);
         }
-
-        newCollectionObject(collectionBody, collectionClass, collectionCount);
-
-        $(this).parent().siblings('td.parameter')
-                    .children('table.parameters')
-                    .children('tbody')
-                    .append(collectionBody.children());
     });
 
-// So the added collections do have the event handler thing,
-// it's that they do not have the 'collection-original' class, and so they are 
-// being deleted before they can be added.
-// It seems like getting the class of the row from which was clicked on
-// and using that inplace of 'collection-original'... should work...
+    // What should collectionBody be? li elements from ul.parameters
     function newCollectionObject(collectionBody, collectionClass, collectionCount) {
         // Rows
         collectionBody.children().each(function() {
-            var type = $(this).children('td.type').text();
+            var type = $(this).children('ul').children('li.type').text();
             type = type.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+
 
             if (!$(this).hasClass(collectionClass)) {
                 $(this).remove();
@@ -343,9 +353,9 @@
                 $(this).removeClass(collectionClass);
                 $(this).addClass('collection-new-'+collectionCount);
                 if (type == 'collection') {
-                    newCollectionObject($(this).children('td.parameter') 
-                                            .children('table.parameters')
-                                            .children('tbody'),
+                    newCollectionObject($(this).children('ul.collection')
+                                        .children('li.parameter')
+                                        .children('ul.parameters'),
                                           collectionClass,
                                           collectionCount
                                         );
@@ -355,39 +365,24 @@
     }
 
     //
-    // Adding addition fields for list type
-    // 
-    // First thing, add 'add' button to parameters of type 'list'
-    $("td.type:contains('list')").each(function() {
-        $(this).siblings('td.name')
-            .append("<br/><a href='#' class='add-list button add' onclick='return false'><img src='/images/plus.png'></img></a>");
-    });
-
-    // Add additional fields for list type element.
-    $('.add-list').click(function() {
-        var originalList = $(this).parent()
-                                    .siblings('td.parameter')
-                                    .find('tr.list-element');
-        
-        // Add the new list
-        $(this).parent().siblings('td.parameter')
-            .children('table.parameters')
-            .children('tbody')
-            .append(originalList.get(0).outerHTML);
-    });
-
-    //
     // Minimize functionality for collections and lists
     // 
-    $('td').on("click", "a.collection-minimize, a.list-minimize", function(event) {
+    $('li').on("click", "a.collection-minimize, a.list-minimize", function(event) {
         event.stopPropagation();
         // Get the class of the following row
-        var minimize_class = $(this).parent().parent().parent().parent().next().attr('class');
-        // trim whitespace
-        minimize_class = minimize_class.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
-        // Minimize all following rows of the same class.
-        $(this).parent().parent().parent().parent().siblings('.' + minimize_class).slideToggle();
-
+        var minimize_class = $(this).parent().parent().parent().next().attr('class');
+        // Object case
+        if (minimize_class === undefined) {
+            $(this).parent().parent().parent().siblings().slideToggle();
+        }
+        // Collection case
+        else {
+            // trim whitespace
+            minimize_class = minimize_class.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+            // Minimize all following rows of the same class.
+            $(this).parent().parent().parent().siblings('.' + minimize_class).slideToggle();
+        }
+        // Rotating the minimize button
         if ($(this).parent().hasClass('rotate')) {
             $(this).parent().removeClass('rotate');
         }
