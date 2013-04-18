23eec998614cb0762b412de5c8564f382eb28630
Vijay Korapaty <vijay.korapaty@whitehatsec.com>
2013-04-18 14:02:09 -0700

    Front end works as desired.

--- public/javascripts/editing.js
+++ public/javascripts/editing.js
@@ -1,6 +1,53 @@
 (function() {
     $('.edit').on("click", function(event) {
         event.stopPropagation();
-        console.log($(this).siblings());
+        editField($(this));
     });
+
+    $('.cancel-edit').on("click", function(event) {
+        event.stopPropagation();
+        $(this).siblings('.edit').show();
+        $(this).hide();
+        $(this).siblings('.save-edit').hide();
+        getEditElement($(this)).children('textarea').remove();
+    });
+
+    $('.save-edit').on("click", function(event) {
+        event.stopPropagation();
+        $(this).siblings('.edit').show();
+        $(this).hide();
+        $(this).siblings('.cancel-edit').hide();
+        var editElem = getEditElement($(this));
+        var updatedField = editElem.children('textarea').val();
+        editElem.children('textarea').remove();
+        editElem.replaceWith(updatedField);
+    });
+
+    function editField (editIcon) {
+        var editElem = getEditElement(editIcon);
+        var editText = editElem[0].outerHTML;
+        editElem.append('<br><textarea>'+ editText +'</textarea>');
+
+        // Hide edit icon, show save and cancel icons.
+        editIcon.hide();
+        editIcon.siblings('.save-edit').show();
+        editIcon.siblings('.cancel-edit').show();
+    }
+
+    function getEditElement (icon) {
+        if (icon.parent().is('form')) {
+            return icon.siblings('span.description');
+        }
+        else if (icon.parent().is('td.description, li.description')) {
+            return icon.siblings().filter('p');
+        }
+        else {
+            console.log('Unexpected element.');
+            console.log(icon);
+            console.log('parent.');
+            console.log(icon.parent());
+            console.log('parent of parent.');
+            console.log(icon.parent().parent());
+        }
+    }
 })();
